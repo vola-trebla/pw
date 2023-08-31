49cb14d01a68cb099635fb3998d5d3fd661a396b
albert.alov <albert.alov@ncloudtech.ru>
2023-08-31 16:28:13 +0300

    TASK-3 | feat: added first structure and simple test

---
+++ src/pages/base-page.js
@@ -0,0 +1,22 @@
+const { test } = require('@playwright/test');
+const { MainForm } = require('../components/main-form');
+const { DonateForm } = require('../components/donate-form');
+
+/**
+ * Базовая страница, в ней содержатся все формы, а так же возможность перейти на этй страницу
+ */
+class BasePage {
+  constructor(page) {
+    this.mainForm = new MainForm(page);
+    this.donateForm = new DonateForm(page);
+    this._page = page;
+  }
+
+  async visit(url) {
+    await test.step(`Открываем url "${url}"`, async () => {
+      await this._page.goto(url, { waitUntil: 'networkidle' });
+    });
+  }
+}
+
+exports.BasePage = BasePage;
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,9 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+const { test } = require('../src/fixtures/fixtures');
+test.beforeEach(async ({ basePage }) => {
+  await basePage.visit('/qa-test-7R58U3/');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('simple test', async ({ basePage }) => {
+  await basePage.mainForm.openDonateForm();
+  await basePage.donateForm.monthlyButton.click();
 });
